ce8f5952ecd6fc780959fd07fe00f766cd516188
larnaa <juliakorni1997@yandex.ru>
2022-10-08 13:52:58 -0300

    Add stop and run app + get user_is

--- src/bluecoins_cli/adb.py
+++ src/bluecoins_cli/adb.py
@@ -11,12 +11,30 @@ if not device_list:
 elif len(device_list) >= 2:
     raise Exception('Found two or more devices. Connect only one device.')
 
-for info in device_list:
-    device_id = info.serial
+device = adb.device(serial=device_list[0].serial)
+
+print(device)
+print(device.serial)
+
+
+# stop bluecoins
+device.shell(f'am force-stop {APP_ID}')
+device.shell(f'pm disable-user --user 0 {APP_ID}')
+
+
+user_response = device.shell(f'dumpsys package {APP_ID} | grep userId')
+# split to list, take second element, str --> int
+user_id = int(user_response.split(sep='=')[1])
+
+
+# run bluecoins
+device.shell(f'pm enable {APP_ID}')
+
 
 #   export DB="bluecoins-$(date +%s)"
-# 	adb shell am force-stop ${APP_ID}
-#   adb shell dumpsys package com.example.myapp | grep userId=
+# 	+ adb shell am force-stop ${APP_ID}
+#   + adb shell pm disable-user --user 0 com.rammigsoftware.bluecoins
+#   + adb shell dumpsys package com.rammigsoftware.bluecoins | grep userId
 # 	adb shell su 10128 -c "cat /data/user/0/com.rammigsoftware.bluecoins/databases/bluecoins.fydb" > ${DB}.fydb
 # 	poetry run bluecoins-cli ${DB}.fydb convert
 # 	adb push ${DB}.new.fydb /data/local/tmp/${DB}.new.fydb
@@ -24,3 +42,4 @@ for info in device_list:
 # 	adb shell su 0 -c mv /data/local/tmp/${DB}.new.fydb /data/user/0/com.rammigsoftware.bluecoins/databases/bluecoins.fydb
 # 	adb shell su 0 -c mv /data/local/tmp/${DB}.fydb /data/user/0/com.rammigsoftware.bluecoins/databases/${DB}.fydb
 # 	adb shell am start -n com.rammigsoftware.bluecoins/.ui.activities.main.MainActivity
+#   + adb shell pm enable com.rammigsoftware.bluecoins
