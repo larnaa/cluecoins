db5bc52fb675eca73e101e9b6a44414457cd64b1
larnaa <juliakorni1997@yandex.ru>
2022-10-12 17:23:28 -0300

    review

--- src/bluecoins_cli/adb.py
+++ src/bluecoins_cli/adb.py
@@ -4,11 +4,17 @@ import subprocess
 from adbutils import adb  # type: ignore
 from adbutils._device import AdbDevice  # type: ignore
 
+APP_ID = 'com.rammigsoftware.bluecoins'
+
+
+def get_db_name() -> str:
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    return f"bluecoins-{current_time}"
+
 
 class Device:
     def __init__(self, device: AdbDevice) -> None:
-        self.DEVICE = device
-        self.APP_ID = 'com.rammigsoftware.bluecoins'
+        self.device = device
 
     @classmethod
     def connect(cls) -> 'Device':
@@ -22,23 +28,18 @@ class Device:
         device = adb.device(serial=device_list[0].serial)
         return Device(device)
 
-    @staticmethod
-    def create_db() -> str:
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-        return f"bluecoins-{current_time}"
-
     def stop_app(self) -> None:
-        self.DEVICE.app_stop(self.APP_ID)
-        self.DEVICE.shell(f'pm disable-user --user 0 {self.APP_ID}')
+        self.device.app_stop(APP_ID)
+        self.device.shell(f'pm disable-user --user 0 {APP_ID}')
 
     def get_app_user_id(self) -> int:
-        user_response = self.DEVICE.shell(f'dumpsys package {self.APP_ID} | grep userId')
+        user_response = self.device.shell(f'dumpsys package {APP_ID} | grep userId')
         # NOTE: split to list, take second element, str --> int
         return int(user_response.split(sep='=')[1])
 
     def pull_db(self, db: str) -> None:
         subprocess.run(
-            f'adb shell su {self.APP_ID} -c "cat /data/user/0/{self.APP_ID}/databases/bluecoins.fydb" > {db}.fydb',
+            f'adb shell su {APP_ID} -c "cat /data/user/0/{APP_ID}/databases/bluecoins.fydb" > {db}.fydb',
             shell=True,
             check=True,
         )
@@ -48,24 +49,22 @@ class Device:
 
     def push_db_root(self, db: str) -> None:
 
-        self.DEVICE.sync.push(f'{db}.new.fydb', f'/data/local/tmp/{db}.new.fydb')
+        self.device.sync.push(f'{db}.new.fydb', f'/data/local/tmp/{db}.new.fydb')
         # FIXME: don't use root
-        self.DEVICE.shell(
-            f'su 0 -c mv /data/local/tmp/{db}.new.fydb /data/user/0/{self.APP_ID}/databases/bluecoins.fydb'
-        )
+        self.device.shell(f'su 0 -c mv /data/local/tmp/{db}.new.fydb /data/user/0/{APP_ID}/databases/bluecoins.fydb')
 
-        self.DEVICE.sync.push(f'{db}.fydb', f'/data/local/tmp/{db}.fydb')
+        self.device.sync.push(f'{db}.fydb', f'/data/local/tmp/{db}.fydb')
         # FIXME: don't use root
-        self.DEVICE.shell(f'su 0 -c mv /data/local/tmp/{db}.fydb /data/user/0/{self.APP_ID}/databases/{db}.fydb')
+        self.device.shell(f'su 0 -c mv /data/local/tmp/{db}.fydb /data/user/0/{APP_ID}/databases/{db}.fydb')
 
     def start_app(self, activity: str) -> None:
-        self.DEVICE.shell(f'pm enable {self.APP_ID}')
-        self.DEVICE.app_start(self.APP_ID, activity)
+        self.device.shell(f'pm enable {APP_ID}')
+        self.device.app_start(APP_ID, activity)
 
 
 device = Device.connect()
 device.stop_app()
-db = device.create_db()
+db = get_db_name()
 device.pull_db(db)
 device.cli_command_run('convert', db)
 device.push_db_root(db)
