bea618b806bbf85f7afcbb226efa9016fa7be941
larnaa <juliakorni1997@yandex.ru>
2022-10-15 11:15:45 -0300

    Add button "exit programm"

--- src/bluecoins_cli/tui.py
+++ src/bluecoins_cli/tui.py
@@ -1,4 +1,7 @@
+import sys
+
 from pytermgui.file_loaders import YamlLoader
+from pytermgui.widgets.button import Button
 from pytermgui.widgets.containers import Container
 from pytermgui.widgets.input_field import InputField
 from pytermgui.window_manager.manager import WindowManager
@@ -31,26 +34,24 @@ with YamlLoader() as loader:
     loader.load(PYTERMGUI_CONFIG)
 
 with WindowManager() as manager:
+    main_window = Window(width=60, box="DOUBLE")
     window = (
-        Window(
-            "",
-            InputField(
+        (
+            main_window
+            + ""
+            + InputField(
                 "A CLI tool to manage the database of Bluecoins,\nan awesome budget planner for Android.",
                 multiline=True,
-            ),
-            "",
-            Container(
+            )
+            + ""
+            + Container(
                 "In development:",
                 InputField("- archive"),
                 box="EMPTY_VERTICAL",
-            ),
-            "",
-            [
-                "Convert",
-                lambda *_: execute_cli_command_with_adb('convert', '.ui.activities.main.MainActivity'),
-            ],
-            width=60,
-            box="DOUBLE",
+            )
+            + ""
+            + Button('Convert', lambda *_: execute_cli_command_with_adb('convert', '.ui.activities.main.MainActivity'))
+            + Button('Exit programm', lambda *_: sys.exit(0))
         )
         .set_title("[210 bold]Bluecoins CLI")
         .center()
