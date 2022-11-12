899e0fa9230d5e19502b5bf6964d7a05eac049de
larnaa <juliakorni1997@yandex.ru>
2022-11-12 20:14:50 -0300

    added query tests

--- tests/test_query_database.py
+++ tests/test_query_database.py
@@ -1,13 +1,20 @@
 import sqlite3
-from sqlite3 import Connection
-import pytest
 from pathlib import Path
+from sqlite3 import Connection
+from typing import Generator
+
+import pytest
 
 from cluecoins.database import create_new_account
+from cluecoins.database import delete_account
+from cluecoins.database import find_account
+from cluecoins.database import get_base_currency
+from cluecoins.database import set_base_currency
+
 
 @pytest.fixture
-def create_memory_db() -> Connection:
-    """ Fixture to set up the in-memory database with test data """
+def create_memory_db() -> Generator[Connection]:
+    """Fixture to set up the in-memory database with test data"""
 
     conn = sqlite3.connect(':memory:')
 
@@ -16,15 +23,46 @@ def create_memory_db() -> Connection:
     conn.executescript(sql)
 
     yield conn
-    
 
-def test_create_new_account(create_memory_db: Connection):
+
+def test_set_base_currency(create_memory_db: Connection) -> None:
+    # TODO: check current currency ?
 
     conn = create_memory_db
-    cursor = conn.cursor()
+    set_base_currency(conn, 'UYU')
 
+    base_currency = conn.cursor().execute(
+        'SELECT defaultSettings FROM SETTINGSTABLE WHERE settingsTableID = "1"',
+    )
+    expected_base_currency = base_currency.fetchone()[0]
+
+    assert expected_base_currency == 'UYU'
+
+
+def test_get_base_currency(create_memory_db: Connection) -> None:
+
+    conn = create_memory_db
+    expected_base_currency = get_base_currency(conn)
+
+    assert expected_base_currency == 'USD'
+
+
+def test_find_account(create_memory_db: Connection) -> None:
+    # TODO: add an account within this function
+
+    conn = create_memory_db
+    account = find_account(conn, 'Visa')
+    expected_account = account[1]
+
+    assert expected_account == 'Visa'
+
+
+def test_create_new_account(create_memory_db: Connection) -> None:
+
+    conn = create_memory_db
     create_new_account(conn, 'FTX', 'USD')
-    account = cursor.execute(
+
+    account = conn.cursor().execute(
         'SELECT * FROM ACCOUNTSTABLE WHERE accountName = ?',
         ('FTX',),
     )
@@ -32,3 +70,17 @@ def test_create_new_account(create_memory_db: Connection):
 
     assert expected_account_name == 'FTX'
 
+
+def test_delete_account(create_memory_db: Connection) -> None:
+    # TODO: add and delete an account within this function
+
+    conn = create_memory_db
+    delete_account(conn, 1)
+
+    account = conn.cursor().execute(
+        'SELECT * FROM ACCOUNTSTABLE WHERE accountsTableID = ?',
+        (1,),
+    )
+    expected_account = account.fetchall()
+
+    assert expected_account == []
